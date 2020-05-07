6b2f3a73b18b14be33af9fe413f4d2908e0eddf0
heoseok <heoseok87@gmail.com>
2020-05-07 18:18:37 +0900

    쿠키 추가. 마지막 segment만 실패한 경우, 성공한 것으로 간주

--- src/main/java/net/chrislongo/hls/PlaylistDownloader.java
+++ src/main/java/net/chrislongo/hls/PlaylistDownloader.java
@@ -20,142 +20,196 @@ package net.chrislongo.hls;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PlaylistDownloader {
-    private URL url;
-    private List<String> playlist;
-    private Crypto crypto;
+	private URL url;
+	private List<String> playlist;
+	private Crypto crypto;
+	private String cookie;
 
-    private static String EXT_X_KEY = "#EXT-X-KEY";
-    private static final String BANDWIDTH = "BANDWIDTH";
+	private static String EXT_X_KEY = "#EXT-X-KEY";
+	private static final String BANDWIDTH = "BANDWIDTH";
 
-    public PlaylistDownloader(String playlistUrl) throws MalformedURLException {
-        this.url = new URL(playlistUrl);
-        this.playlist = new ArrayList<String>();
-    }
+	Pattern pattern = Pattern.compile("BANDWIDTH=([0-9]+)");
 
-    public void download(String outfile) throws IOException {
-        this.download(outfile, null);
-    }
+	public PlaylistDownloader(String playlistUrl) throws MalformedURLException {
+		this(playlistUrl, null);
+	}
 
-    public void download(String outfile, String key) throws IOException {
-        fetchPlaylist();
+	public PlaylistDownloader(String playlistUrl, String cookie) throws MalformedURLException {
+		this.url = new URL(playlistUrl);
+		this.playlist = new ArrayList<String>();
+		this.cookie = cookie;
+	}
 
-        this.crypto = new Crypto(getBaseUrl(this.url), key);
+	public void download(String outfile) throws IOException {
+		this.download(outfile, null);
+	}
 
-        for (String line : playlist) {
-            line = line.trim();
+	public void download(String outfile, String key) throws IOException {
+		fetchPlaylist();
 
-            if (line.startsWith(EXT_X_KEY)) {
-                crypto.updateKeyString(line);
+		while (new File(outfile).exists()) {
+			Path path = Paths.get(outfile);
+			outfile = Paths.get(path.getParent().toString(), path.getFileName().toString().replace(".", "_."))
+					.toString();
+		}
 
-                System.out.printf("\rCurrent Key: %s                                  \n", crypto.getCurrentKey());
-                System.out.printf("Current IV:  %s\n", crypto.getCurrentIV());
-            } else if (line.length() > 0 && !line.startsWith("#")) {
-                URL segmentUrl;
+		System.out.println("Download to " + outfile);
 
-                if (!line.startsWith("http")) {
-                    String baseUrl = getBaseUrl(this.url);
-                    segmentUrl = new URL(baseUrl + line);
-                } else {
-                    segmentUrl = new URL(line);
-                }
+		this.crypto = new Crypto(getBaseUrl(this.url), key);
 
-                downloadInternal(segmentUrl, outfile);
-            }
-        }
+		List<URL> urlList = new ArrayList<URL>();
 
-        System.out.println("\nDone.");
-    }
+		for (String line : playlist) {
+			line = line.trim();
 
-    private void downloadInternal(URL segmentUrl, String outFile) throws IOException {
-        byte[] buffer = new byte[512];
+			if (line.startsWith(EXT_X_KEY)) {
+				crypto.updateKeyString(line);
 
-        InputStream is = crypto.hasKey()
-                ? crypto.wrapInputStream(segmentUrl.openStream())
-                : segmentUrl.openStream();
+				// System.out.printf("\rCurrent Key: %s \n", crypto.getCurrentKey());
+				// System.out.printf("Current IV: %s\n", crypto.getCurrentIV());
+			} else if (line.length() > 0 && !line.startsWith("#")) {
+				URL segmentUrl;
 
-        FileOutputStream out;
+				if (!line.startsWith("http")) {
+					String baseUrl = getBaseUrl(this.url);
+					segmentUrl = new URL(baseUrl + line);
+				} else {
+					segmentUrl = new URL(line);
+				}
+				urlList.add(segmentUrl);
+			}
+		}
 
-        if (outFile != null) {
-            File file = new File(outFile);
-            out = new FileOutputStream(outFile, file.exists());
-        } else {
-            String path = segmentUrl.getPath();
-            int pos = path.lastIndexOf('/');
-            out = new FileOutputStream(path.substring(++pos), false);
-        }
+		for (int i = 0; i < urlList.size(); i++) {
+			try {
+				downloadInternal(urlList.get(i), outfile);
+				System.out.println(String.format("%d / %d", i + 1, urlList.size()));
+			} catch (IOException e) {
+				if (urlList.size() - i < 2) {
+					// 마지막 파일만 실패한 경우 성공으로 간주
+					continue;
+				} else {
+					throw e;
+				}
+			}
+		}
 
-        System.out.printf("Downloading segment: %s\r", segmentUrl);
+		System.out.println("\nDone.");
+	}
 
-        int read;
+	private void downloadInternal(URL segmentUrl, String outFile) throws IOException {
+		byte[] buffer = new byte[512];
 
-        while ((read = is.read(buffer)) >= 0) {
-            out.write(buffer, 0, read);
-        }
+		URLConnection connection = segmentUrl.openConnection();
+		if (hasCookie()) {
+			// System.out.println("Set cookie : " + this.cookie);
+			connection.setRequestProperty("Cookie", this.cookie);
+		}
 
-        is.close();
-        out.close();
-    }
+		InputStream is = crypto.hasKey() ? crypto.wrapInputStream(connection.getInputStream())
+				: connection.getInputStream();
 
-    private String getBaseUrl(URL url) {
-        String urlString = url.toString();
-        int index = urlString.lastIndexOf('/');
-        return urlString.substring(0, ++index);
-    }
+		FileOutputStream out;
 
-    private void fetchPlaylist() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        boolean isMaster = false;
-        long maxRate = 0L;
-        int maxRateIndex = 0;
+		if (outFile != null) {
+			File file = new File(outFile);
+			out = new FileOutputStream(outFile, file.exists());
+		} else {
+			String path = segmentUrl.getPath();
+			int pos = path.lastIndexOf('/');
+			out = new FileOutputStream(path.substring(++pos), false);
+		}
 
-        String line;
-        int index = 0;
+		// System.out.printf("Downloading segment: %s\r", segmentUrl);
 
-        while ((line = reader.readLine()) != null) {
-            playlist.add(line);
+		int read;
 
-            if (line.contains(BANDWIDTH))
-                isMaster = true;
+		while ((read = is.read(buffer)) >= 0) {
+			out.write(buffer, 0, read);
+		}
 
-            if (isMaster && line.contains(BANDWIDTH)) {
-                try {
-                    int pos = line.lastIndexOf("=");
-                    long bandwidth = Long.parseLong(line.substring(++pos));
+		is.close();
+		out.close();
+	}
 
-                    maxRate = Math.max(bandwidth, maxRate);
+	private String getBaseUrl(URL url) {
+		String urlString = url.toString();
+		int index = urlString.lastIndexOf('/');
+		return urlString.substring(0, ++index);
+	}
 
-                    if (bandwidth == maxRate)
-                        maxRateIndex = index + 1;
-                } catch (NumberFormatException ignore) {}
-            }
+	private void fetchPlaylist() throws IOException {
+		URLConnection connection = url.openConnection();
+		if (hasCookie()) {
+			connection.setRequestProperty("Cookie", this.cookie);
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		boolean isMaster = false;
+		long maxRate = 0L;
+		int maxRateIndex = 0;
 
-            index++;
-        }
+		String line;
+		int index = 0;
 
-        reader.close();
+		while ((line = reader.readLine()) != null) {
+			playlist.add(line);
 
-        if (isMaster) {
-            System.out.printf("Found master playlist, fetching highest stream at %dKb/s\n", maxRate / 1024);
-            this.url = updateUrlForSubPlaylist(playlist.get(maxRateIndex));
-            this.playlist.clear();
+			if (line.contains(BANDWIDTH))
+				isMaster = true;
 
-            fetchPlaylist();
-        }
-    }
+			if (isMaster && line.contains(BANDWIDTH)) {
+				try {
+					// int pos = line.lastIndexOf("=");
+					long bandwidth = 0;
+					Matcher matcher = this.pattern.matcher(line);
+					if (matcher.find()) {
+						bandwidth = Long.parseLong(matcher.group(1));
+					}
 
-    private URL updateUrlForSubPlaylist(String sub) throws MalformedURLException {
-        String newUrl;
+					maxRate = Math.max(bandwidth, maxRate);
 
-        if (!sub.startsWith("http")) {
-            newUrl = getBaseUrl(this.url) + sub;
-        } else {
-            newUrl = sub;
-        }
+					if (bandwidth == maxRate)
+						maxRateIndex = index + 1;
+				} catch (NumberFormatException ignore) {
+				}
+			}
 
-        return new URL(newUrl);
-    }
+			index++;
+		}
+
+		reader.close();
+
+		if (isMaster) {
+			System.out.printf("Found master playlist, fetching highest stream at %dKb/s\n", maxRate / 1024);
+			this.url = updateUrlForSubPlaylist(playlist.get(maxRateIndex));
+			this.playlist.clear();
+
+			fetchPlaylist();
+		}
+	}
+
+	private URL updateUrlForSubPlaylist(String sub) throws MalformedURLException {
+		String newUrl;
+
+		if (!sub.startsWith("http")) {
+			newUrl = getBaseUrl(this.url) + sub;
+		} else {
+			newUrl = sub;
+		}
+
+		return new URL(newUrl);
+	}
+
+	public boolean hasCookie() {
+		return this.cookie != null && !this.cookie.trim().isEmpty();
+	}
 }
